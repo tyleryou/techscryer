2b37ff74c6b78d8cda697c0d9239cfa3ddf532d0
Tyler Young <tyler.you@protonmail.com>
2025-06-18 15:27:08 -0500

    Completed logging service in observability toolkit

--- tools/observability.py
+++ tools/observability.py
@@ -11,6 +11,9 @@ from opentelemetry.exporter.otlp.proto.grpc._log_exporter import OTLPLogExporter
 from opentelemetry.sdk.resources import Resource
 from dotenv import load_dotenv
 import os
+import grpc
+import base64
+from grpc import ssl_channel_credentials
 
 
 class Observe:
@@ -20,35 +23,51 @@ class Observe:
         self.resource = Resource.create({
             "service.name": service_name,
         })
+        self.headers = {'authorization': f'Bearer {os.getenv("OTEL_BEARER_TOKEN")}'}
+        self.tls_cert_path = os.getenv('TLS_CERT_PATH')
 
-        # Initialize tracing
-        self._setup_tracing()
+        # # # Initialize tracing
+        # self._setup_tracing()
 
         # Initialize logging
         self._setup_logging()
+        # Bearer token for authentication on server side.
 
-    def _setup_tracing(self):
-        """Configure OpenTelemetry tracing"""
-        trace_provider = TracerProvider(resource=self.resource)
-        trace_provider.add_span_processor(
-            BatchSpanProcessor(
-                OTLPSpanExporter(
-                    endpoint=f"{os.getenv('OTEL_HOST')}:{os.getenv('OTEL_PORT')}",  # Explicit endpoint
-                    insecure=True  # Only for development/testing
-                )
-            )
-        )
-        trace.set_tracer_provider(trace_provider)
-        self.tracer = trace.get_tracer(__name__)
+
+    def _get_tls_credentials(self):
+        """Helper method to load TLS credentials"""
+        with open(self.tls_cert_path, "rb") as f:
+            trusted_certs = f.read()
+        return grpc.ssl_channel_credentials(root_certificates=trusted_certs)
+
+    # def _setup_tracing(self):
+    #     """Configure OpenTelemetry tracing"""
+    #     credentials = self._get_tls_credentials()
+    #     trace_provider = TracerProvider(resource=self.resource)
+    #     trace_provider.add_span_processor(
+    #         BatchSpanProcessor(
+    #             OTLPSpanExporter(
+    #                 endpoint=f"{os.getenv('OTEL_COLLECTOR_HOST')}:{os.getenv('OTEL_COLLECTOR_PORT')}",  # Explicit endpoint
+    #                 # insecure=False,
+    #                 headers=self.headers,
+    #                 credentials=credentials
+    #             )
+    #         )
+    #     )
+    #     trace.set_tracer_provider(trace_provider)
+    #     self.tracer = trace.get_tracer(__name__)
 
     def _setup_logging(self):
         """Configure OpenTelemetry logging"""
+        credentials = self._get_tls_credentials()
         logger_provider = LoggerProvider(resource=self.resource)
         logger_provider.add_log_record_processor(
             BatchLogRecordProcessor(
                 OTLPLogExporter(
-                    endpoint=f"{os.getenv('OTEL_HOST')}:{os.getenv('OTEL_PORT')}",  # Explicit endpoint
-                    insecure=True  # Only for development/testing
+                    endpoint=f"{os.getenv('OTEL_COLLECTOR_HOST')}:{os.getenv('OTEL_COLLECTOR_PORT')}",  # Explicit endpoint
+                    # insecure=False,
+                    headers=self.headers,
+                    credentials=credentials
                 )
             )
         )
@@ -56,12 +75,12 @@ class Observe:
 
         # Configure Python logging
         logging.basicConfig(level=logging.INFO)
-        logger = logging.getLogger()
+        logger = logging.getLogger(__name__)
 
         # Add OpenTelemetry handler
         otel_log_handler = LoggingHandler(
             logger_provider=logger_provider,
-            level=logging.INFO
+            level=logging.INFO,
         )
         logger.addHandler(otel_log_handler)
 
---
+++ tools/pipeline.py
@@ -0,0 +1,80 @@
+import requests
+from tools.observability import Observe
+from typing import Any, Literal
+from pymongo import MongoClient
+
+# Logging to Stdout
+logger = Observe('igdb_pipeline').get_logger()
+print(logger.handlers)
+def load(
+        database_platform: Literal['postgresql', 'mongodb'],
+        database_name: str,
+        table_name: str,
+        host: str,
+        port: int,
+        user: str,
+        password: str,
+        data: [str, Any]
+) -> bool:
+    if database_platform == 'postgres':
+        # do some work
+        return True
+    elif database_platform == 'mongodb':
+        logger.info(f'Loading data into {database_platform}: {database_name}.{table_name}')
+        client = MongoClient(f'mongodb://{user}:{password}@{host}:{port}/')
+        db = client[database_name]
+        collection = db[table_name]
+        try:
+            collection.insert_many(data)
+            logger.info(f'Successfully uploaded data into {database_platform}: {database_name}.{table_name}')
+            return True
+        except Exception as e:
+            logger.error(f'Failed to insert data into {database_platform}: {database_name}.{table_name} --- {e}')
+            raise Exception(f'Failed to insert into MongoDB with error: {e}')
+    else:
+        raise Exception('Correct database not entered.')
+
+
+class Pipeline:
+    def __init__(self, base_url: str):
+        self.base_url = base_url
+        logger.info(f'Pipeline initialized with base URL: {base_url}')
+
+    # Expecting a JSON object return or None if request fails.
+    def extract(
+            self,
+            endpoint: str,
+            client_id: str,
+            token: str,
+            body_query: str,
+            # token_type: str
+    ) -> dict[str, Any] | None:
+        full_url = self.base_url+endpoint
+
+        headers = {
+            'Client-ID': client_id,
+            'Authorization': f'Bearer {token}',
+        }
+
+        logger.info(f'Sending POST request to: [{full_url}]')
+
+        try:
+            r = requests.post(
+                url=full_url,
+                headers=headers,
+                data=body_query
+            )
+            # Raise if unsuccessful post.
+            r.raise_for_status()
+            # Check for empty response
+            if r.json() is not None:
+                logger.info(f'Successfully extracted data from: [{endpoint}]')
+                return r.json()
+            else:
+                logger.error(f'No data received from endpoint: [{endpoint}]')
+                raise Exception(f'No data from endpoint: [{endpoint}]')
+
+        except requests.exceptions.RequestException as e:
+            logger.error(f'Failed on request to endpoint: [{endpoint}] --- {e}')
+
+# To do
